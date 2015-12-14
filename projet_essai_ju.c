15e03ca9442f2879da68ee616618b45e0a441112
jsntju <jsnt.denis@gmail.com>
2015-12-14 13:34:41 +0100

    Update projet_essai_ju.c

--- projet_essai_ju.c
+++ projet_essai_ju.c
@@ -17,6 +17,86 @@
 //Bleu que sur 2 couleurs  differentes
 //Vert que sur 3 couleurs differentes
 
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+/** 
+* Polytech Marseille
+* Case 925 -163, avenue de Luminy
+* 13288 Marseille CEDEX 9
+*
+* Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut être 
+* reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+*/
+
+/**
+* @author DENIS Justine <justine.denis@etu.univ-amu.fr>
+*
+* @version 0.0.1 / 16-09-2015
+* @toto NA
+* @bug NA 
+*/
+
+/**
+* @file essai_1.c
+* @brief
+*
+*  Essai sur le back track pour la base
+**/ 
+
+
+#include <stdio.h>
+
 int main (void){
-  
+// Donnees
+    int i, j, k, k1;
+    int niveau[k][k][k];            //Initialisation tableau
+    int rouge, bleu ,vert;          
+    rouge = 1;
+    bleu = 2;
+    vert = 3;
+    k = 4;
+    
+    
+//Code:
+    //Initialisation des tableaux a 0:
+    for(k1=0; k1<k; k1++){
+       for (i=0; i<k; i++) {
+           for(j=0; j<k; j++){
+            niveau[i][j][k1] = 0;
+            }
+        }
+    }
+    
+    
+    
+    //Affichage des niveaux:
+    for(k1=0; k1<k; k1++){
+        printf("\n Niveaux %i:\n",k1);
+       for (i=0; i<k; i++) {
+        printf("\n");
+           for(j=0; j<k; j++) {
+            printf("%i ",niveau[i][j][k1]);
+            }
+        }
+        printf("\n");
+    }
+    
+    //Creation de la base:
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
